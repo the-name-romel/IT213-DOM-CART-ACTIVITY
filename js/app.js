8afdebdc09b46fe22e8dcf177cd15e7f06ef562b
the-name-romel <rommelbon100@gmail.com>
2021-10-04 17:33:42 +0800

    Removing courses from the cart and clear the cart

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //variables
 const courses = document.querySelector('#courses-list'),
       shoppingCartContent = document.querySelector('#cart-content tbody');
+      clearCartBtn = document.querySelector('#clear-cart');
 
 
 //listeners
@@ -8,8 +9,14 @@ const courses = document.querySelector('#courses-list'),
 loadEventListeners();
 
 function loadEventListeners() {
-
+    //adding course to cart
     courses.addEventListener('click', buyCourse) 
+    
+    //when the remove button is clicked
+    shoppingCartContent.addEventListener('click', removeCourse);
+
+    //clear cart button
+    clearCartBtn.addEventListener('click', clearCart);
 }
 
 
@@ -54,4 +61,19 @@ function addIntoCart(course) {
     `;
     // add into the shopping cart
     shoppingCartContent.appendChild(row);
+}
+
+//remove course from the dom
+function removeCourse(e) {
+
+    if(e.target.classList.contains('remove')) {
+        e.target.parentElement.parentElement.remove();
+    }
+}
+// clears the cart
+function clearCart(e) {
+    e.preventDefault();
+    while(shoppingCartContent.firstChild) {
+        shoppingCartContent.removeChild(shoppingCartContent.firstChild);
+    }
 }
